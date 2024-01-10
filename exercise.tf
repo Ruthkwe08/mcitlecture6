fc25ae557ab7d7d5f4d2f94c4a49e0a167d08e14
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-01-10 18:39:17 -0500

    Create exercise.tf

---
+++ exercise.tf
@@ -0,0 +1,33 @@
+variable "nested_list" {​
+
+  type    = list(list(string))​
+
+  default = [["a", "b"], ["c", "d"], ["e", "f"]]​
+
+}​
+
+variable "nested_map" {​
+
+  type    = map(map(string))​
+
+  default = {​
+
+    group1 = {"name" = "Alice", "age" = "25"}​
+
+    group2 = {"name" = "Bob", "age" = "30"}​
+
+  }​
+
+}​
+
+locals{​
+
+flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])​
+
+}   ​
+
+output "flattened_list_result" {​
+
+  value = local.flattened_list​
+
+}​
