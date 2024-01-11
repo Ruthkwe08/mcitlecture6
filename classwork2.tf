f39167059e7b44e9b15be9ae8f6b63bcf5a74778
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-01-10 20:08:36 -0500

    Update classwork2.tf

--- classwork2.tf
+++ classwork2.tf
@@ -8,6 +8,7 @@ group2={"name"="Bob","age"="30"}
 locals{
 flattened_map = flatten{[for index, character in var.nested_map:[for kk, vv in v: {group=k, key, value=vv}]]
 }
+
 output"flattened_map_result"{
   value=local.flattened_map
 }
