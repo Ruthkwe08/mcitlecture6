16758b0b3e2e5143b542193ce3861f166ddd0cc2
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-01-10 20:09:46 -0500

    Update classwork2.tf

--- classwork2.tf
+++ classwork2.tf
@@ -1,14 +1,14 @@
 variable "nested_map"{
-type =map(map(string))
-default={
-group1={"name"="Alice","age"="25"}
-group2={"name"="Bob","age"="30"}
+type = map(map(string))
+default= {
+group1 = {"name"="Alice","age"="25"}
+group2 = {"name"="Bob","age"="30"}
   }
 }
 locals{
-flattened_map = flatten{[for index, character in var.nested_map:[for kk, vv in v: {group=k, key, value=vv}]]
+flattened_map = flatten{[for index, character in var.nested_map:[for kk, vv in v: {group = k, key, value = vv}]]
 }
 
 output"flattened_map_result"{
-  value=local.flattened_map
+  value = local.flattened_map
 }
